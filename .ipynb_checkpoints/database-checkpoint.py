3108b15073f51b25516efb194cf900a0c58c8226
REudR <rramirezeudave@tudelft.nl>
2026-06-18 12:40:39 +0200

    Delete .ipynb_checkpoints/database-checkpoint.py

--- .ipynb_checkpoints/database-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import Base
-
-engine = create_engine("sqlite:///cordhisk.db")
-Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
